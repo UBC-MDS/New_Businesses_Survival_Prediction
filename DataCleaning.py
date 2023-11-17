bc3e56b4fd1cd16d693b88f97b4d4d144ce515ac
Beth Ou Yang <beth.ouyang@gmail.com>
2023-11-16 23:46:09 -0800

    fix conflicts from main

--- DataCleaning.py
+++ DataCleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-def datacleaning(business, survival_threshold):
+def business_datacleaning(self, business, survival_threshold):
     # Drop rows where ExpiredDate and IssuedDate are NA
     business = business.dropna(subset = ["ExpiredDate", "IssuedDate"])
 
@@ -23,7 +23,21 @@ def datacleaning(business, survival_threshold):
     # Filter to keep those records where the latest `ExpiredDate` is before or equal to year 2022.
     business = business[business['ExpiredDate'] <= dt.date(2022, 12, 31)]
 
+    # Adjust format of FOLDERYEAR
+    business['FOLDERYEAR'] = business['FOLDERYEAR'].apply(lambda x : '20' + str(x))
+    
+    # Create target colum and adjust Boolean to 0, 1
     business['survival_status'] = business['survival_days'] >= survival_threshold
-    # Adjust Boolean to 0, 1
     business["survival_status"] = business["survival_status"].astype(int)
     return business
+
+def gdp_datacleaning(gdp):
+    gdp = gdp[gdp['North American Industry Classification System (NAICS)'] == 'All industries [T001]'][['REF_DATE', 'VALUE']]
+    gdp['REF_YEAR'] = gdp['REF_DATE'].apply(lambda x : str(x)[:4])
+    gdp = gdp[gdp['REF_YEAR'] >= 2012]
+    gdp['REF_DATE'] = pd.to_datetime(gdp['REF_DATE'])
+    gdp['REF_YEAR'] = gdp['REF_YEAR'].astype(str)
+    return gdp.rename(columns = {'VALUE': 'GdpValue', 'REF_YEAR': 'FOLDERYEAR'})
+
+def merge_business_econ_by_year(business, econ):
+    return business.merge(econ, on='FOLDERYEAR', how='inner')
