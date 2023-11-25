070201a733ba08335490772b2cf82a2de8648a5c
Beth Ou Yang <beth.ouyang@gmail.com>
2023-11-25 00:10:08 -0800

    docstring for eda part

--- src/EDA.py
+++ src/EDA.py
@@ -3,6 +3,16 @@ alt.data_transformers.enable("vegafusion")
 import matplotlib.pyplot as plt
 
 def numeric_feature_visualization(data, features):
+    """
+    Generates a grid of density plots for numeric features, grouped by survival status.
+
+    Parameters:
+    - data (pandas.DataFrame): The input DataFrame containing numeric features and the 'survival_status' column.
+    - features (list of str): List of column names corresponding to numeric features in the DataFrame.
+
+    Returns:
+    - An Altair LayerChart displaying density plots for numeric features grouped by survival status.
+    """
     charts_numeric = [alt.Chart(data).transform_density(
         feature,
         as_=[feature, 'density'],
@@ -23,6 +33,16 @@ def numeric_feature_visualization(data, features):
     return chart_grid
 
 def large_variance_numeric_feature_visualization(data, feature):
+    """
+    Generates a density plot for a numeric feature with a restricted x-axis domain, grouped by survival status.
+
+    Parameters:
+    - data (pandas.DataFrame): The input DataFrame containing the numeric feature and the 'survival_status' column.
+    - feature (str): The column name corresponding to the numeric feature in the DataFrame.
+
+    Returns:
+    - An Altair Chart displaying a density plot for the specified numeric feature with a restricted x-axis domain.
+    """
     chart = alt.Chart(data).transform_density(
         feature,
         as_=[feature, 'density'],
@@ -39,6 +59,16 @@ def large_variance_numeric_feature_visualization(data, feature):
     return chart
 
 def categorical_feature_visualization(data, feature):
+    """
+    Generates a faceted bar chart for a categorical feature, colored by survival status.
+
+    Parameters:
+    - data (pandas.DataFrame): The input DataFrame containing the categorical feature and the 'survival_status' column.
+    - feature (str): The column name corresponding to the categorical feature in the DataFrame.
+
+    Returns:
+    - An Altair Chart displaying a faceted bar chart for the specified categorical feature grouped by survival status.
+    """
     chart = alt.Chart(data).mark_bar(opacity=0.5).encode(
         alt.X(feature, sort='-y').stack(False),
         y='count()',
@@ -50,6 +80,16 @@ def categorical_feature_visualization(data, feature):
     return chart
 
 def varianced_categorical_feature_visualization(data, feature):
+    """
+    Generates a faceted bar chart for a categorical feature with high variance, colored by survival status.
+
+    Parameters:
+    - data (pandas.DataFrame): The input DataFrame containing the categorical feature and the 'survival_status' column.
+    - feature (str): The column name corresponding to the categorical feature in the DataFrame.
+
+    Returns:
+    - An Altair Chart displaying a faceted bar chart for the specified categorical feature with high variance, grouped by survival status.
+    """
     top_20_categories = data[feature].value_counts().head(20).index.tolist()
     filtered = data[data[feature].isin(top_20_categories)]
 
