8fb20264d251641271518b6676c2fae907b457ac
prabh34 <prabhjit.thind95@gmail.com>
2023-12-01 22:34:25 -0800

    updated evaluation and model file

--- src/Modeling.py
+++ src/Modeling.py
@@ -18,12 +18,13 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LogisticRegression
 
-def transform(df, word_features, categorical_features, numeric_features):
+def transform(df, X_test, y_test, test_data_to, word_features, categorical_features, numeric_features):
     """
     Transform and preprocess a DataFrame with different types of features.
 
     Parameters:
     - df (pandas.DataFrame): The input DataFrame containing the features to be transformed.
+    - test_data_to (str): Path to save the test data
     - word_features (list of str): List of column names corresponding to text features in the DataFrame.
     - categorical_features (list of str): List of column names corresponding to categorical features in the DataFrame.
     - numeric_features (list of str): List of column names corresponding to numeric features in the DataFrame.
@@ -55,6 +56,15 @@ def transform(df, word_features, categorical_features, numeric_features):
     word_trans_arr = word_transformer.fit_transform(df[word_features])
     categorical_trans_arr = categorical_transformer.fit_transform(df[categorical_features])
     numeric_trans_arr = numeric_transformer.fit_transform(df[numeric_features])
+
+    word_trans_arr_test = word_transformer.transform(X_test[word_features])
+    categorical_trans_arr_test = categorical_transformer.transform(X_test[categorical_features])
+    numeric_trans_arr_test = numeric_transformer.transform(X_test[numeric_features])
+
+    X_test_transformed = np.hstack((word_trans_arr_test.toarray(), categorical_trans_arr_test, numeric_trans_arr_test))
+    X_test_transformed = pd.DataFrame(X_test_transformed)
+    print(len(X_test_transformed))
+    X_test_transformed.to_csv(os.path.join(test_data_to, "scaled_test.csv"), index=False)
     
     return np.hstack((word_trans_arr.toarray(), categorical_trans_arr, numeric_trans_arr))
 
@@ -106,24 +116,28 @@ def main(business_data, test_data_to, pipeline_to, seed):
     }
     
     X_train, y_train = split_x_y(train_df, **features)
+
     X_test, y_test = split_x_y(test_df, **features)
     
-    X_train_transformed = transform(X_train, **features)
-    X_test_transformed = transform(X_test, **features)
+    X_train_transformed = transform(X_train, X_test, y_test, test_data_to, **features)
 
     bnb = BernoulliNB()
     pd.DataFrame(cross_validate(bnb, X_train_transformed, y_train, cv=10, return_train_score=True))
 
     logreg = LogisticRegression(random_state=seed, max_iter=1000)
-    pd.DataFrame(cross_validate(logreg, X_train_transformed, y_train, cv=10, return_train_score=True))
+    print("Cross Validation Results:")
+    cross_validate_df = pd.DataFrame(cross_validate(logreg, X_train_transformed, y_train, cv=10, return_train_score=True))
+    print(cross_validate_df)
 
-    logreg.fit(X_test_transformed, y_test)
+    logreg.fit(X_train_transformed, y_train)
 
     model_path = pipeline_to + "lr_license_renewal_pipeline.pickle"
     with open(os.path.join(model_path), 'wb') as f:
         pickle.dump(logreg, f)
-    
-    print(logreg.score(X_test_transformed, y_test))
+
+    print("\n")
+    print("Train score:", logreg.score(X_train_transformed, y_train))
+
 
 if __name__ == "__main__":
     main()
